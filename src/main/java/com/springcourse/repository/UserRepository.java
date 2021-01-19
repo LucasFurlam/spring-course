dae937e745675904e36b98c7d3b6adadc647e3c4
Lucas Furlam <lucasfurlanfreitas@gmail.com>
2021-01-19 19:54:11 -0300

    Create user repository

---
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.springcourse.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	public Optional<User> findByEmailAndPassword(String email, String password);
+
+}
